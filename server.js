8a859f43b96a0b65da00aea62ba0911b48ff5a20
divyaranjan parida <lucky7400547947@gmail.com>
2024-05-17 15:00:06 +0530

    basic structure added

---
+++ server.js
@@ -0,0 +1,29 @@
+import express, { response } from "express";
+import mongoose from "mongoose";
+import cors from 'cors';
+
+
+// dotenv.config()
+
+// express app
+const app = express();
+const port = process.env.PORT || 3000
+
+const MONGO_URI = process.env.MONGO_URI;
+// console.log(MONGO_URI);
+
+app.use(cors({
+    origin: "http://localhost:5173/"
+}));
+app.use(express.json());
+
+const connectDb = async () => {
+    try {
+        const connect = await mongoose.connect(MONGO_URI);
+        console.log("Db connected...");
+    } catch (error) {
+        console.log("Db is not connected!!!", error);
+    }
+}
+
+connectDb();
